f18da3ca8af2acfdee108c5371a13b8f19fd322d
nhanlnt-96 <nhanlnt@hotmail.com>
2022-03-05 01:39:49 +0700

    loginScreen: complete create login screen UI

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,11 @@
 package main;
 
+import screens.login.LoginScreen;
+
 import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
-    JPanel jPanel = new JPanel();
-    JFrame jFrame = new JFrame();
 
-    jFrame.setSize(1024, 768);
-    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    jFrame.add(jPanel);
-
-    jPanel.setLayout(null);
-
-    jFrame.setVisible(true);
   }
 }
--- src/screens/login/LoginScreen.java
+++ src/screens/login/LoginScreen.java
@@ -1,11 +1,28 @@
 package screens.login;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class LoginScreen {
+public class LoginScreen extends JFrame {
   private JPanel loginScreen;
+  private JPanel loginHeader;
+  private JPanel loginContainer;
   private JTextField usernameInput;
   private JPasswordField passwordInput;
+  private JComboBox positionInput;
   private JButton loginButton;
-  private JButton forgotPasswordButton;
+
+  public LoginScreen() {
+
+  }
+
+  public static void main(String[] args) {
+    LoginScreen lS = new LoginScreen();
+    lS.setTitle("Login");
+    lS.getContentPane().add(lS.loginScreen, BorderLayout.CENTER);
+    lS.setSize(790, 410);
+    lS.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    lS.setVisible(true);
+  }
+
 }
