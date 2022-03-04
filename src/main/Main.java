fd8a2b45a805e284bbbab5cefa7f3bbaf558798b
nhanlnt-96 <nhanlnt@hotmail.com>
2022-03-05 00:39:13 +0700

    loginScreen: complete create login screen UI

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("This is Groups 3 Repo");
+    JPanel jPanel = new JPanel();
+    JFrame jFrame = new JFrame();
+
+    jFrame.setSize(1024, 768);
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    jFrame.add(jPanel);
+
+    jPanel.setLayout(null);
+
+    jFrame.setVisible(true);
   }
 }
---
+++ src/screens/login/LoginScreen.java
@@ -0,0 +1,11 @@
+package screens.login;
+
+import javax.swing.*;
+
+public class LoginScreen {
+  private JPanel loginScreen;
+  private JTextField usernameInput;
+  private JPasswordField passwordInput;
+  private JButton loginButton;
+  private JButton forgotPasswordButton;
+}
